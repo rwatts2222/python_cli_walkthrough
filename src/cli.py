bb609bd2e9834ba0161561328442783068baa5d0
rwatts2222 <rmwatts@bsu.edu>
2021-01-06 16:25:11 -0500

    started working/finished item.py

---
+++ src/cli.py
@@ -0,0 +1,71 @@
+"""
+Work wants an inventory app that:
+    Stores data into a file
+    Uses the command line to list/add/update/delete:
+        "Items" they have:
+            id
+            name
+            cond
+            ?checkedIn?
+"""
+
+next_id = 0
+items = [1, 2, 3]
+# TODO Make a menu print out showing options
+def menu():
+    print("""
+1. List All Items
+2. Add New Item
+3. Update Existing Item
+4. Delete Item (By item id)
+5. Exit
+""")
+
+# List All Items
+def list_items():
+    print("in list item function")
+    for item in items:
+        print(item)
+    print("in list item function")
+# Add New Item
+def new_item():
+    global next_id
+    name = input("Name:")
+    cond = input("Condition: ")
+    item_id = next_id
+    next_id += 1
+
+# Update Existing Item
+def update_existing(itemId):
+    pass
+
+# Delete Item (By item id)
+def delete_item(itemId):
+    pass
+
+# Make the menu questions that grab the data
+while True:
+    menu()
+    choice = input("> ")
+    
+    if choice == "1":
+        list_items()
+        pass
+    elif choice == "2":
+        new_item()
+    elif choice == "3":
+        pass
+    elif choice == "4":
+        pass
+    # Exit
+    elif choice == "5":
+        exit()
+    else:
+        input("Invalid Input!\n(Press Enter to try again)")
+
+
+
+
+
+
+# Make the File Saving stuff
