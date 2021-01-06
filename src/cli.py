2b6119547131ec3134b0a18376bcdfe71c521fb1
rwatts2222 <rmwatts@bsu.edu>
2021-01-06 16:44:35 -0500

    finished 2/4 crud functions

--- src/cli.py
+++ src/cli.py
@@ -1,6 +1,6 @@
 """
 Work wants an inventory app that:
-    Stores data into a file
+    Stores Data into a file
     Uses the command line to list/add/update/delete:
         "Items" they have:
             id
@@ -10,7 +10,7 @@ Work wants an inventory app that:
 from models.item import Item
 
 next_id = 0
-items = [1, 2, 3]
+items = []
 # TODO Make a menu print out showing options
 def menu():
     print("""
@@ -21,25 +21,25 @@ def menu():
 5. Exit
 """)
 
-# List All Items
+# List all Items
 def list_items():
     for item in items:
         print(item)
-    
+
 # Add New Item
 def new_item():
     global next_id
     global items
 
-    name = input("Name:")
+    name = input("Name: ")
     cond = input("Condition: ")
     item_id = next_id
 
     next_id += 1
 
-    
     tmp = Item(item_id, name, cond)
     items.append(tmp)
+
 
 # Update Existing Item
 def update_existing(itemId):
@@ -49,29 +49,27 @@ def update_existing(itemId):
 def delete_item(itemId):
     pass
 
-# Make the menu questions that grab the data
-while True:
-    menu()
-    choice = input("> ")
-    
-    if choice == "1":
-        list_items()
-        pass
-    elif choice == "2":
-        new_item()
-    elif choice == "3":
-        pass
-    elif choice == "4":
-        pass
-    # Exit
-    elif choice == "5":
-        exit()
-    else:
-        input("Invalid Input!\n(Press Enter to try again)")
+# Make the menu questions that grab the data 
+def main():
+    while True:
+        menu()
+        choice = input("> ")
 
-
-
-
+        if choice == "1":
+            list_items()
+        elif choice == "2":
+            new_item()
+        elif choice == "3":
+            pass
+        elif choice == "4":
+            pass
+        elif choice == "5": # Exit
+            exit()
+        else:
+            input("Invalid Input!\n(Press Enter to try again)")
 
 
 # Make the File Saving stuff
+
+if __name__ == "__main__":
+    main()
